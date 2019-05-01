c72baaf588527f7eae01e1f657ba35669e2a2767
monkog <monkog@wp.pl>
2019-05-01 21:47:24 +0200

    Add type extensions unit tests

---
+++ BinaryFilesReaderTests/Extensions/TypeExtensionsTests.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using BinaryFilesReader.Extensions;
+using NUnit.Framework;
+
+namespace BinaryFilesReaderTests.Extensions
+{
+	[TestFixture]
+	public class TypeExtensionsTests
+	{
+		[Test]
+		[TestCase(typeof(bool))]
+		[TestCase(typeof(int))]
+		[TestCase(typeof(double))]
+		[TestCase(typeof(float))]
+		[TestCase(typeof(long))]
+		[TestCase(typeof(short))]
+		[TestCase(typeof(string))]
+		public void IsSimpleType_SimpleTypes_True(Type type)
+		{
+			var result = type.IsSimpleType();
+
+			Assert.IsTrue(result);
+		}
+
+		[Test]
+		[TestCase(typeof(object))]
+		[TestCase(typeof(TypeExtensionsTests))]
+		public void IsSimpleType_ComplexTypes_False(Type type)
+		{
+			var result = type.IsSimpleType();
+
+			Assert.IsFalse(result);
+		}
+
+		[Test]
+		public void ConvertObject_CanConvert_InstanceReturned()
+		{
+			var result = typeof(TypeExtensionsTests).ConvertObject(this, out _);
+
+			Assert.IsNotNull(result);
+		}
+
+		[Test]
+		public void ConvertObject_CanConvert_NoErrorMessage()
+		{
+			typeof(TypeExtensionsTests).ConvertObject(this, out var error);
+
+			StringAssert.AreEqualIgnoringCase(string.Empty, error);
+		}
+
+		[Test]
+		public void ConvertObject_CannotConvert_InstanceReturned()
+		{
+			var result = typeof(TypeExtensions).ConvertObject(this, out _);
+
+			Assert.IsNull(result);
+		}
+
+		[Test]
+		public void ConvertObject_CannotConvert_NoErrorMessage()
+		{
+			typeof(TypeExtensions).ConvertObject(this, out var error);
+
+			StringAssert.AreNotEqualIgnoringCase(string.Empty, error);
+		}
+	}
+}
